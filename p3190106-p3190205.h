8f988322b22dd12d0cc3c344dc88f8159f2400bf
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-04-27 21:35:09 +0300

    moved functions to .h

--- p3190106-p3190205.h
+++ p3190106-p3190205.h
@@ -6,7 +6,7 @@
 #define N_OVEN 10
 #define N_DELIVERER    7
 #define T_ORDER_LOW    1
-#define T_ORDER_HI1GH   5
+#define T_ORDER_HIGH   5
 #define N_ORDER_LOW    1
 #define N_ORDER_HIGH   5
 #define T_PAYMENT_LOW  1
@@ -18,5 +18,48 @@
 #define T_PACK  2
 #define T_DEL_LOW  5
 #define T_DEL_HIGH 15
+
+typedef pthread_cond_t condv;
+typedef pthread_mutex_t mutex;
+typedef pthread_t thread;
+
+mutex out_lock;
+mutex pay_lock;
+
+unsigned int rand_seed = 869;
+
+void init_helper_vars() {
+	pthread_mutex_init(&out_lock, NULL);
+	pthread_mutex_init(&pay_lock, NULL);
+}
+
+/* logstr and logerr both append a newline in the end */
+void logstr(char* string) {
+	pthread_mutex_lock(&out_lock);
+	printf("%s\n",string);
+	fflush(stdout);
+	pthread_mutex_unlock(&out_lock);
+}
+
+/* Printing both in stdout or stderr won't cause a crash but might print messages in the same line */
+void logerr(char* string) { 
+	pthread_mutex_lock(&out_lock);
+	fprintf(stderr, "%s\n",string);
+	fflush(stderr);
+	pthread_mutex_unlock(&out_lock);
+}
+
+int randint(int start, int end) {
+	if(rand_seed == 869)
+		logerr("Warning: no seed initialized.\nTODO: REMOVE THIS");
+	return (rand_r(&rand_seed) % (end-start+1)) + start;
+}
+
+/* Increase `sum` by `amt`  s a f e l y */
+void pay(int amt, int* sum) {
+	pthread_mutex_lock(&pay_lock);
+	*sum += amt;
+	pthread_mutex_unlock(&pay_lock);
+}
 
 #endif
