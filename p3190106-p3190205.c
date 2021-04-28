9001e06c03031112677fb9c13913705d7df60846
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-04-28 11:01:04 +0300

    removed a crap ton of declarations, made readable
    it's only two files, just open .h and see the functions
    better prints
    #ifdef DEBUG
    different exit codes for different errors
    init_helper_mutexes first call so logerr can be called

--- p3190106-p3190205.c
+++ p3190106-p3190205.c
@@ -5,42 +5,33 @@
 
 #include "p3190106-p3190205.h"
 
-#define DEBUG
+// #define DEBUG
+#define MAX_LOG_LENGTH 100
 
 int customers;
 long* ids;
 thread* threads;
+
+/* global counters for stats */
 int moneyyy = 0;
 
-condv tele_condv;
+/* global mutexes and condition variables */
 mutex tele_mutex;
+condv tele_condv;
+
+/* global counters for available resourses */
 int available_telephone_guys = N_TELE;
 
-/* Initializes stuff of the .h file*/
-void init_helper_vars();
-
-/*Prints a string in a thread-safe way to stdout*/
-void logstr(char* string);
-
-/*Prints a string in a thread-safe way to stderr*/
-void logerr(char* string);
-
-/*Returns a random integer between start and end (inclusive)*/
-int randint(int start, int end);
-
-/* increases sum by amt  s a f e l y */
-void pay(int amt, int* sum);
-
-/*Starts the pizza order*/
-void* startOrder(void* args);
-
-
+/* Starts the pizza order */
 void* startOrder(void* args) {
-	// buffer for sprintf and logstr
-	char msg[100];
 
+	/* buffer for sprintf and logstr for this thread */
+	char msg[MAX_LOG_LENGTH];
+
+#ifdef DEBUG
 	sprintf(msg, "Thread %ld started", (long) args);
 	logstr(msg);
+#endif
 
 	/* Wait for telephone guy */
 	while (available_telephone_guys == 0)
@@ -48,104 +39,112 @@ void* startOrder(void* args) {
 
 	--available_telephone_guys;
 	
-	/* Order pizzas */
+	/* Select pizzas */
 	int pizza_count = randint(N_ORDER_LOW, N_ORDER_HIGH);
 
 	/* Pay for pizzas */
-	unsigned int s = randint(T_PAYMENT_LOW, T_PAYMENT_HIGH);
-	sprintf(msg, "thread %ld will sleep for %d\n", (long) args, s);
+	unsigned int sleep_time = randint(T_PAYMENT_LOW, T_PAYMENT_HIGH);
+#ifdef DEBUG
+	sprintf(msg, "Order %ld waits for %d seconds on the phone", (long) args, sleep_time);
 	logstr(msg);
-	sleep(s);	
+#endif
+	sleep(sleep_time);	
 
-	/* Fail to pay for pizzas*/
+	/* Fail to pay for pizzas */
 	if (randint(0, 1 * 100000) < P_FAIL * 100000) {
 		sprintf(msg, "Order %ld failed", (long) args);
 		logstr(msg);
-		// exit with code 1 so sum of codes = number of failed
+		/* exit with code 1 so sum of codes = number of failed */
 		pthread_exit((void*) 1);
-	}		
-	sprintf(msg, "Order %ld kataxwrh8hke lmao", (long) args);
-	logstr(msg);
+	}
 
 	/* Actually pay for pizzas */
-	pay(C_PIZZA * pizza_count, &moneyyy);
+	sprintf(msg, "Order %ld registered", (long) args);
+	logstr(msg);
+	increment(C_PIZZA * pizza_count, &moneyyy);
 
 	/* Free the telephone guy */
+	++available_telephone_guys;
 	pthread_mutex_unlock(&tele_mutex);
 	pthread_cond_signal(&tele_condv);
-	++available_telephone_guys;
 
-	/* ... */
+	/*
+	 * ...
+	 * other stuff I don't have to worry about
+	 * ...
+	 */
 	pthread_exit((void*) 0);
 }
 
 
 int main(int argc, char** argv) {
 
+	/* Initialize all mutexes / cond variables */
+	init_helper_mutexes();
+	pthread_mutex_init(&tele_mutex, NULL);
+
 #ifdef DEBUG
 	printf("Customers: %s\nSeed: %s\n", argv[1],argv[2]);
 #endif
 
-	/*Initialize program arguments*/
-	if(argc != 3){
-		fprintf(stderr,"Error: Give the number of customers and a random seed as arguments.");
-		exit(-1);
+	/* Initialize program arguments */
+	if (argc != 3) {
+		logerr("Error: Give the number of customers and a random seed as arguments.");
+		exit(1);
 	}
 
 	/* Customer Number */
 	int cust_num = atoi(argv[1]);
 	if (cust_num <= 0) {
-		logerr("Error: Invalid number of customers.");
-		exit(-1);
+		logerr("Error: there must be at least one customer.");
+		exit(2);
 	}
 	customers = cust_num;
 
 	ids = (long*) calloc(cust_num, sizeof(long));
 	if (ids == NULL) {
 		logerr("Error: memory allocation in id array");
-		exit(-1);
+		exit(3);
 	}
 
 	threads = (thread*) malloc(cust_num * sizeof(thread));
 	if (threads == NULL) {
 		logerr("Error: memory allocation in thread array");
-		exit(-1);
+		exit(4);
 	}
 
 	/* Random Seed */
 	rand_seed = atoi(argv[1]);
 
-	/*Initialize all mutexes / cond variables*/
-	init_helper_vars();
-	pthread_mutex_init(&tele_mutex, NULL);
-
-	/*Initialize threads*/
-	for (long i = 0; i < customers; i++) {
+	/* Initialize orders */
+	for (long i = 0; i < customers; ++i) {
 		ids[i] = i;
 		if (pthread_create(&threads[i], NULL, startOrder, (void*) ids[i])) {
-			logerr("Error: Thread could not be created");
+			fprintf(stderr, "Error: Thread %ld could not be created", i);
 		}
 		sleep(randint(T_ORDER_LOW, T_ORDER_HIGH));
 	}
 
-	/*Wait for threads to finish*/
+	/* Wait for orders to finish */
 	int failed = 0;
         void* status;
 	for (int i = 0; i < customers; i++) {
 		pthread_join(threads[i], &status);
+#ifdef DEBUG
 		char msg[100];
 		sprintf(msg, "thread %d exited with code %ld\n", i, (long)
 				status);
 		logstr(msg);
+#endif
 		failed += (long) status;
 	}
 
-	/* Print useful stats */
-	printf("money: %d, succ: %d, fail: %d\n", moneyyy, cust_num - failed, failed);
+	/* Print very useful stats */
+	printf("\nVery Useful Stats:\n");
+	printf("Total revenue: %d\nSuccessful orders: %d\nFailed orders: %d\n", moneyyy, cust_num - failed, failed);
 
-	/* flex */
+	/* we know `free` exists pls give good grade */
 	free(threads);
 	free(ids);
 	return 0;
 }
-
