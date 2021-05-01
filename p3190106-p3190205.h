6c9edf316f406b9b01a4c4f1054d2dc57da56c15
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-05-01 12:43:27 +0300

    used the required time method, better comments

--- p3190106-p3190205.h
+++ p3190106-p3190205.h
@@ -12,39 +12,48 @@
 #define T_PAYMENT_LOW  1
 #define T_PAYMENT_HIGH 2
 #define C_PIZZA 10
-#define P_FAIL  0.05
+#define P_FAIL 0.05
 #define T_PREP  1
 #define T_BAKE  10
 #define T_PACK  2
 #define T_DEL_LOW  5
 #define T_DEL_HIGH 15
 
-#define DEBUG
 #define MAX_LOG_LENGTH 100
 
 typedef pthread_t thread;
 typedef pthread_mutex_t mutex;
 typedef pthread_cond_t condv;
 
+unsigned int rand_seed;
+
+/* struct with information about an order.
+ * used to facilitate organisation of code into functions. */
 typedef struct {
 	long threadID;
 	int num_of_pizzas;
-	time_t order_start_time;
-	time_t order_baked_time;
+	struct timespec order_start_time;
+	struct timespec order_baked_time;
 } pizza_info;
 
-unsigned int rand_seed = 869;
+/* Function called by all threads to make an order.
+ * Successively executes all steps for the pizza delivery. */
+void* make_order(void*);
 
+/* one function for each part of the order, for each resource.
+ * they all share the same information about an order. */
 int order_pizzas(pizza_info*);
 void prepare_pizzas(pizza_info*);
 void cook_pizzas(pizza_info*);
 void package_pizzas(pizza_info*);
 void deliver_pizzas(pizza_info*);
 
+/* ==================== Utility Functions ==================== */
+
 /*
  * logstr and logerr both append a newline in the end.
  * use locks so that the lines are not scrambled.
- * spinlocks are fine because printing is (relatively) very quick.
+ * spinlocks are fine because printing is very quick.
  */
 void logstr(char*);
 void logerr(char*);
@@ -52,13 +61,13 @@ void logerr(char*);
 /* returns random integer in the range [start, end] */
 int randint(int start, int end);
 
-/* increments `total` by `amt`  s a f e l y */
+/* increments `total` by `amt` */
 void increment(int amt, int* total);
 
-/* sets `max` to the max of `max` and `val` */
+/* sets `max` to the maximum of `val` and `max` */
 void max(int val, int* max);
 
-/* returns the time elapsed from `start_time` to now */
-time_t time_elapsed(time_t start_time);
+/* returns the time elapsed from start to now */
+int time_elapsed(struct timespec*);
 
 #endif
