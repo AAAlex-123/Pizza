cc396c88699bda761353dd697b6514077360c76a
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-05-12 16:00:19 +0300

    documentation for  e v e r y t h i n g

--- test-res.sh
+++ test-res.sh
@@ -1,9 +1,15 @@
+# remove old file to prevent execution on failed compilation
 rm pizza.out
+
+# compile
 gcc -o pizza.out -pthread -Wall -Wextra -Werror p3190106-p3190205.c
 
+# optionally use commandline arguments
 if [ -z "$1" ]
 then
+	# use default number of customers
 	./pizza.out 100 1000
 else
+	# use user-provided number of customers
 	./pizza.out $1 1000
 fi
