6b4243992f05a0655493c42a4b82c148809a45c5
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-05-15 20:28:32 +0300

    Final no debug (#5)
    * removed debug
    * documentation for  e v e r y t h i n g
    * Rename p3190106-p3190205.c to p3190106-p3190205-pizza.c
    * Rename p3190106-p3190205.h to p3190106-p3190205-pizza.h
    * better .sh

--- test-res.sh
+++ test-res.sh
@@ -1,9 +1,15 @@
-rm pizza.out
-gcc -o pizza.out -pthread -Wall -Wextra -Werror p3190106-p3190205.c
+# silently remove old file to prevent execution on failed compilation
+rm -f pizza.out
 
+# compile
+gcc -o pizza.out -pthread -Wall -Wextra -Werror p3190106-p3190205-pizza.c
+
+# optionally use commandline arguments
 if [ -z "$1" ]
 then
+	# use default number of customers
 	./pizza.out 100 1000
 else
+	# use user-provided number of customers
 	./pizza.out $1 1000
 fi
